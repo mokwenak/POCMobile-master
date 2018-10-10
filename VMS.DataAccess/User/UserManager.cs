0692d7219fcdafab2b1cbe528d525d6f042c1f13
mokwena kgwete <kwenzz@hotmail.com>
2018-10-10 17:26:53 +0200

    Add project files.

---
+++ VMS.DataAccess/User/UserManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace VMS.DataAccess
+{
+    public class UserManager
+    {
+        private Vehicle_ManagementEntities Context;
+
+        public UserManager()
+        {
+            this.Context = new Vehicle_ManagementEntities();
+        }
+        public UserManager(Vehicle_ManagementEntities context)
+        {
+            this.Context = context;
+        }
+
+        public bool ValidateUser(string username,string password)
+        {
+            MembershipUser u;         
+
+            try
+            {   ////////////////ASPNET default authentication
+
+                u = Membership.GetUser(username);
+
+                if (Membership.ValidateUser(username, password))
+                {
+                    return true;
+
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ VMS.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VMS.DataAccess;
+
+namespace VMS.WebAPI.Controllers
+{
+
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        UserManager userManager;
+        public UserController()
+        {
+            this.userManager = new UserManager();
+        }
+        public UserController(UserManager usermanager)
+        {
+            this.userManager = usermanager;
+        }
+
+        [HttpGet,Route("login")]
+        public IHttpActionResult Login(string usr,string pwd)
+        {
+            var result = this.userManager.ValidateUser(usr, pwd);
+
+            return Ok(result);
+        }
+    }
+}
